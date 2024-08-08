690dd10269bb89c3e7920645b84ed2c8e702a3c4
immark007 <immark5507@gmail.com>
2024-08-08 16:27:48 -0300

    Adicionando um evento para o input cpf

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+function formatCPF(cpf) {
+    cpf = cpf.replace(/\D/g, ''); 
+    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
+    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    return cpf;
+}
+
+function onCPFInput(event) {
+    let input = event.target;
+    input.value = formatCPF(input.value);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let cpfInput = document.getElementById('cpf');
+    cpfInput.addEventListener('input', onCPFInput);
+});
